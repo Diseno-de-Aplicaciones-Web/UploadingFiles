2ea653b3008089df894e47fa4801fe2aeddb2471
DanBaDo <byte4life@pm.me>
2022-05-24 20:30:21 +0200

    S3 example

--- CrazyExpressDistributedFiles/index.mjs
+++ CrazyExpressDistributedFiles/index.mjs
@@ -1,21 +1,37 @@
 import Express from 'express'
 import { config } from 'dotenv'
+import aws from 'aws-sdk'
 
 if (process.env.NODE_ENV !== "production") config()
 
+const s3 = new aws.S3({
+    apiVersion: process.env.S3_API_VERSION,
+    endpoint: process.env.S3_ENDPOINT,
+    signatureVersion: process.env.S3_SIGNATURE_VERSION,
+})
+
+class Article {
+    constructor({description, pvp}) {
+        this.id = Date.now()
+        this.description = description
+        this.pvp = pvp
+    }
+    imageIpfsCid = null
+    imageKey = null
+}
+
 const mockedDB = [
     {
-        id: 33,
+        imageIpfsCid: "bafkreid455xi3qvpsmxk5vqmrs3vkbmu3zxlw4qei3vvrz4oaw7t6b34d4",
+        imageKey: "1653408470042",
+        id: 1653414926644,
         description: "Fashion chungle dungle",
-        pvp: 28.95,
-        imageIpfsCid: "bafkreid455xi3qvpsmxk5vqmrs3vkbmu3zxlw4qei3vvrz4oaw7t6b34d4"
+        pvp: 28.95
     }
 ]
 
 const app = new Express()
-// Manejamos el contenido de la petición sin formato
-// como un buffer.
-// app.use(Express.text())
+//app.use(Express.raw())
 
 app.get("/images/", (req,res)=>{
     res.send(
@@ -30,18 +46,47 @@ app.get("/images/", (req,res)=>{
     )
 })
 
-app.post("/upload-image/",(req, res)=>{
-    const imgId = Date.now()
-    const file = fs.createWriteStream(`./uploads/${imgId}.jpg`)
-    console.log(imgId);
-    file.on("close", ()=>{ console.log("Done") })
-    req.on("data",(chunk)=>{console.log("*->")})
-    req.pipe(file)
-    res.sendStatus(200)
-    console.log(fs.readdirSync("./uploads/"))
+app.post("/articles/",Express.json(),(req, res)=>{
+    const article = new Article(req.body)
+    res.json(article)
+    mockedDB.push(article)
 })
 
+app.get("/articles/",(req, res)=>{
+    res.json(mockedDB)
+})
 
+app.post("/upload/:articleId", async (req, res)=>{
+    try {
+        const key = Date.now().toString()
+        let bytesCounter = 0
+        console.log(`Uploading file ${key} - ${req.headers['content-type']} ...`)
+        req.on("data",chunk=>console.log(`${bytesCounter+=chunk.length} bytes...`))
+        req.on("end",()=>console.log(`Done: ${bytesCounter} bytes.`))
 
+        const data = await s3.upload({
+            Body: req,
+            Key: key,
+            ContentType: req.headers['content-type'],
+            Bucket: process.env.S3_BUCKET
+        }).promise()
+
+        const headers = await s3.headObject({
+            Key: key,
+            Bucket: process.env.S3_BUCKET
+        }).promise()
+
+        const articleIdx = mockedDB.findIndex(
+            item => item.id.toString() === req.params.articleId
+        )
+        mockedDB[articleIdx].imageKey = data.Key
+        mockedDB[articleIdx].imageIpfsCid = headers.Metadata.cid
+        res.json({data, headers})
+    } catch (err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
+
+})
 
 app.listen(3000)
