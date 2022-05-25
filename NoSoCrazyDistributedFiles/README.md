959a2b47eb83d85c66d976fd90452b366260c2bf
DanBaDo <byte4life@pm.me>
2022-05-25 11:42:10 +0200

    Simplified S3 version

---
+++ NoSoCrazyDistributedFiles/README.md
@@ -0,0 +1,2 @@
+# AWS SDK for JavaScript - S3
+Ejemplos en la documentación de Amazon: https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html
---
+++ NoSoCrazyDistributedFiles/index.mjs
@@ -0,0 +1,89 @@
+import Express from 'express'
+import { config } from 'dotenv'
+import aws from 'aws-sdk'
+
+if (process.env.NODE_ENV !== "production") config()
+
+const s3 = new aws.S3({
+    apiVersion: process.env.S3_API_VERSION,
+    endpoint: process.env.S3_ENDPOINT,
+    signatureVersion: process.env.S3_SIGNATURE_VERSION,
+})
+
+class Article {
+    constructor({description, pvp}) {
+        this.id = Date.now()
+        this.description = description
+        this.pvp = pvp
+    }
+    imagePublicURL = null
+    imageKey = null
+}
+
+const mockedDB = [
+    {
+        imagePublicURL: "https://public-images.s3.filebase.com/1653471032188",
+        imageKey: "1653408470042",
+        id: 1653414926644,
+        description: "Fashion chungle dungle",
+        pvp: 28.95
+    }
+]
+
+const app = new Express()
+
+app.get("/images/", (req,res)=>{
+    res.send(
+        mockedDB.map(
+            item => `
+                <img 
+                src="${item.imagePublicURL}" 
+                alt="Outra vaca no millo"
+                >
+            `
+        ).join('\n')
+    )
+})
+
+app.post("/articles/",Express.json(),(req, res)=>{
+    const article = new Article(req.body)
+    res.json(article)
+    mockedDB.push(article)
+})
+
+app.get("/articles/",(req, res)=>{
+    res.json(mockedDB)
+})
+
+app.post("/upload/:articleId", async (req, res)=>{
+    try {
+
+        const s3ObjectKey = Date.now().toString()
+
+        const s3Response = s3.upload({ //s3.putObject()
+            Bucket: process.env.S3_BUCKET,
+            Key: s3ObjectKey,
+            Body: req,
+            ContentType: req.headers['content-type'],
+            ContentLength: req.headers['content-length']
+        })
+
+        const data = await s3Response.promise()
+
+        const articleIdx = mockedDB.findIndex(
+            item => item.id.toString() === req.params.articleId
+        )
+
+        mockedDB[articleIdx].imageKey = data.Key
+        mockedDB[articleIdx].imagePublicURL = data.Location
+
+        res.sendStatus(201)
+        
+    } catch (err) {
+        console.error(err)
+        res.sendStatus(500)
+    }
+
+})
+
+app.listen(process.env.PORT, ()=>console.log("Ready..."))
